ee0f4fd0f7229a153eaf405d2bf8261a6c37104d
卢凯 <601819595@qq.com>
2018-12-08 15:59:52 +0800

    小程序写接口

---
+++ data/classiFication.js
@@ -0,0 +1,153 @@
+var categoryData={
+	"code":1,
+	"success":"ok",
+	"data":[
+		{
+			"title":"户外",
+			"list":[
+				{
+					"subList":[
+						{
+							descTitlt:"运动",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/d37612cd-a31e-43e1-a4bc-470f2c224279.png"
+						},
+						{
+							descTitlt:"瑜伽",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/38f8915f-da3f-4a30-98c4-3c5fa16b516f.png"
+						},
+						{
+							descTitlt:"男鞋",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/38f8915f-da3f-4a30-98c4-3c5fa16b516f.png"
+						},
+						{
+							descTitlt:"女鞋",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/e80bf7ca-fdea-4d88-8c68-3b68150d170b.png"
+                        },
+                        {
+                            descTitlt: "女鞋",
+                            descUrl: "https://img01.camel.com.cn/product_class/20170922/e80bf7ca-fdea-4d88-8c68-3b68150d170b.png"
+                        },
+                        {
+                            descTitlt: "女鞋",
+                            descUrl: "https://img01.camel.com.cn/product_class/20170922/e80bf7ca-fdea-4d88-8c68-3b68150d170b.png"
+                        },
+                        {
+                            descTitlt: "女鞋",
+                            descUrl: "https://img01.camel.com.cn/product_class/20170922/e80bf7ca-fdea-4d88-8c68-3b68150d170b.png"
+                        },
+                        {
+                            descTitlt: "女鞋",
+                            descUrl: "https://img01.camel.com.cn/product_class/20170922/e80bf7ca-fdea-4d88-8c68-3b68150d170b.png"
+                        }
+
+					]
+				}
+			]
+		},
+		{
+            "title":"运动",
+			"list":[
+				{
+					"subList":[
+						{
+                            descTitlt:"运动",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/e80bf7ca-fdea-4d88-8c68-3b68150d170b.png"
+						},	
+						{
+                            descTitlt:"运动",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/3ac4aaab-2af4-49ab-82a8-44f4e5d342de.png"
+						}
+					]
+				}
+			]
+		},
+		{
+			"title":"瑜伽",
+			"list":[
+				{
+					"subList":[
+						{
+                            descTitlt:"瑜伽",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/3ac4aaab-2af4-49ab-82a8-44f4e5d342de.png"
+						},
+						{
+                            descTitlt:"瑜伽",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/38f8915f-da3f-4a30-98c4-3c5fa16b516f.png"
+						}
+					]
+				}
+			]
+		},
+		{
+			"title":"男鞋",
+			"list":[
+				{
+					"subList":[
+						{
+                            descTitlt:"男鞋",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/38f8915f-da3f-4a30-98c4-3c5fa16b516f.png"
+						},
+						{
+                            descTitlt:"男鞋",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/e80bf7ca-fdea-4d88-8c68-3b68150d170b.png"
+						}
+					]
+				}
+			]
+		},
+		{
+			"title":"女鞋",
+			"list":[
+				{
+					"subList":[
+						{
+                            descTitlt:"女鞋",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/d37612cd-a31e-43e1-a4bc-470f2c224279.png"
+						},
+						{
+                            descTitlt:"女鞋",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/38f8915f-da3f-4a30-98c4-3c5fa16b516f.png"
+						}
+					]
+				}
+			]
+		},
+		{
+			"title":"童装",
+			"list":[
+				{
+					"subList":[
+						{
+                            "title": "童装",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/e80bf7ca-fdea-4d88-8c68-3b68150d170b.png"
+						},
+						{
+                            "title": "童装",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/3ac4aaab-2af4-49ab-82a8-44f4e5d342de.png"
+						}
+					]
+				}
+			]
+		},
+		{
+			"title":"男装",
+			"list":[
+				{
+					"subList":[
+						{
+                            descTitlt:"男装",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/e80bf7ca-fdea-4d88-8c68-3b68150d170b.png"
+						},
+						{
+                            descTitlt:"男装",
+                            descUrl:"https://img01.camel.com.cn/product_class/20170922/d37612cd-a31e-43e1-a4bc-470f2c224279.png"
+						}
+					]
+				}
+			]
+		},
+	
+	
+	]
+}
+export default categoryData;
